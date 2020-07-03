27251964dc62766e2e872b22ce1f166239d455b3
Mike Izbicki <mike@izbicki.me>
2020-07-02 17:13:14 -0700

    update dependencies for much faster korean parsing

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -12,7 +12,7 @@ with open('tests/input.csv', 'rt', encoding='utf-8', newline='\n') as f:
     inputs = list(csv.DictReader(f, dialect='excel', strict=True))
 
 # filter the input languages to 
-test_langs = ['en','ko','ja','zh']
+test_langs = None #['en','ko','ja','zh']
 if test_langs is not None:
     inputs = [ input for input in inputs if input['lang'] in test_langs ]
 
@@ -27,8 +27,7 @@ pspacy.load_all_langs(langs)
 
 @pytest.mark.parametrize('test', inputs, ids=[ input['lang'] for input in inputs ])
 def test__lemmatize(test,benchmark):
-    benchmark(pspacy.lemmatize,test['lang'],test['text']*10)
-    #benchmark(pspacy.lemmatize,test['lang'],test['text'],False,False,False,False)
+    benchmark(pspacy.lemmatize,test['lang'],test['text'])
 
 
 #@pytest.mark.parametrize('test', inputs, ids=[ input['lang'] for input in inputs ])
