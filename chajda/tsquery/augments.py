e6e66b017cbd4c98f8d35b163271c635aef039d4
Joey Bodoia <jbodoia21@cmc.edu>
2021-05-28 18:58:57 -0700

    implemented augments_fasttext function, currently failing doctests because of downloading model from fasttext
    added fasttext to requirements file
    
    added stdout/stderr suppressor, doctests should be passing now
    
    enabled github actions
    
    testing github actions
    
    testing github actions again
    
    github actions test
    
    Only testing one language in doctest, github actions should pass now

--- chajda/tsquery/augments.py
+++ chajda/tsquery/augments.py
@@ -2,7 +2,20 @@
 '''
 
 from chajda.tsvector import lemmatize, Config
-from chajda.tsquery import to_tsquery
+from chajda.tsquery.__init__ import to_tsquery
+
+
+from contextlib import contextmanager,redirect_stderr,redirect_stdout
+from os import devnull
+
+@contextmanager
+def suppress_stdout_stderr():
+    """Context manager that redirects stdout and stderr to devnull"""
+    with open(devnull, 'w') as fnull:
+        with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+            yield (err, out)
+
+
 
 
 def augments_gensim(lang, word, config=Config(), n=5):
@@ -29,6 +42,14 @@ def augments_gensim(lang, word, config=Config(), n=5):
     ['explosive', 'weaponry', 'gun', 'ammunition', 'device']
     '''
 
+    # load the model if it's not already loaded
+    try:
+        augments_gensim.model
+    except AttributeError:
+        import gensim.downloader
+        with suppress_stdout_stderr():
+            augments_gensim.model = gensim.downloader.load("glove-wiki-gigaword-50")
+
     # find the most similar words;
     try:
         topn = augments_gensim.model.most_similar(word, topn=n+1)
@@ -46,10 +67,51 @@ def augments_gensim(lang, word, config=Config(), n=5):
     return words
 
 
-# load the model if it's not already loaded
-try:
-    augments_gensim.model
-except AttributeError:
-    import gensim.downloader
-    augments_gensim.model = gensim.downloader.load("glove-wiki-gigaword-50")
+import fasttext
+import fasttext.util
 
+fasttext_models = {}
+
+def augments_fasttext(lang, word, config=Config(), n=5):
+    ''' 
+    Returns n words that are "similar" to the input word in the target language.
+    These words can be used to augment a search with the Query class.
+
+    Note: Due to the size of fasttext models, testing multiple languages in the doctest requires more space than github actions allows for. For this reason, only english is tested in the doctest.
+
+    >>> to_tsquery('en', 'baby boy', augment_with=augments_fasttext)
+    '(baby:A | newborn:B | infant:B) & (boy:A | girl:B | boyhe:B | boyit:B)'
+
+    >>> to_tsquery('en', '"baby boy"', augment_with=augments_fasttext)
+    'baby:A <1> boy:A'
+
+    >>> to_tsquery('en', '"baby boy" (school | home) !weapon', augment_with=augments_fasttext)
+    '(baby:A <1> boy:A) & ((school:A | schoo:B | schoolthe:B | schoool:B | kindergarten:B) | (home:A | house:B | homethe:B | homewhen:B | homethis:B)) & !(weapon:A | weaponthe:B | weopon:B)'
+
+    >>> augments_fasttext('en','weapon', n=5)
+    ['weaponthe', 'weopon']
+
+    >>> augments_fasttext('en','king', n=5)
+    ['queen', 'kingthe']
+    '''
+
+    try:
+        fasttext_models[lang]
+    except:
+        with suppress_stdout_stderr():
+            fasttext.util.download_model(lang, if_exists='ignore')
+        fasttext_models[lang] = fasttext.load_model('cc.{0}.300.bin'.format(lang))
+
+    #find the most similar words
+    try:
+        topn = fasttext_models[lang].get_nearest_neighbors(word, k=n)
+        words = ' '.join([ word for (rank, word) in topn ])
+    except KeyError:
+        return []
+
+    # lemmatize the results so that they'll be in the search document's vocabulary
+    words = lemmatize(lang, words, add_positions=False, config=config).split()
+    words = list(filter(lambda w: len(w)>1 and w != word, words))[:n]
+
+
+    return words
