fix numerical error

chajda/embeddings.py
@@ -386,10 +386,12 @@ class Embedding():
                     x = mat @ vector
                     pos_dist = np.linalg.norm(vector - pos_vector)
                     neg_dist = np.linalg.norm(vector - neg_vector)
+                    # FIXME: why are the max/min needed here?  Is it only minor numerical stability issues or is there a real problem with the formula?
+                    cos_sim = min(max(np.dot(x,zero)/np.linalg.norm(x)/np.linalg.norm(zero), 1), -1)
                     if pos_dist > neg_dist:
-                        projection = -math.acos(np.dot(x,zero)/np.linalg.norm(x)/np.linalg.norm(zero))/normalizer
+                        projection = -math.acos(cos_sim)/normalizer
                     else:
-                        projection = math.acos(np.dot(x,zero)/np.linalg.norm(x)/np.linalg.norm(zero))/normalizer
+                        projection = math.acos(cos_sim)/normalizer
                     return mod_result(word, projection)
 
         return (projector, unknown_words)